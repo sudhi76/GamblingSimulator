b10d723016608946f4bd3c309f71c8e172c0de36
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-09 13:06:05 +0530

    Added: Place bet to check win or loss

--- src/com/bridgelabz/demo/GamblingSimulator.java
+++ src/com/bridgelabz/demo/GamblingSimulator.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class GamblingSimulator {
 	static final int STAKE_PER_DAY = 100;
 	static final int BET_PER_DAY = 1;
+	static final int WIN = 1;
 
 	public static void winloss() {
-		int stake = 1;
-		final int WIN = 1;
+		int stake = 0;
 		Random random = new Random();
 		int bet = random.nextInt(2);
 		if (bet == WIN) {
