c4edee4c09720836642522b48a2964c420ec2c67
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-09 12:10:45 +0530

    Added: initialized stake & bet

--- src/gamblingSimulator/GamblingSimulator.java
+++ src/gamblingSimulator/GamblingSimulator.java
@@ -1,6 +1,8 @@
 package gamblingSimulator;
 
 public class GamblingSimulator {
+	static final int STAKE_PER_DAY = 100;
+	static final int BET_PER_DAY = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation");
